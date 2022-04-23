4d51ff0f2076b674a035617d1bf89af2dd2c53b9
your github user namefaisal1205 <mirzfaisa14@yahoo.in>
2022-04-23 21:48:50 +0530

    new

--- index.js
+++ index.js
@@ -75,38 +75,38 @@ function debounce(func,delay)
     },delay)
 }
 
-async function Translate()
-{
-    try{
-         //1.text to translate
-const input = read("inputText") 
-  console.log(input)                           // .de for .com becz we are not using key
-const inputLanguage = read("inputLanguage")
-const outLanguage = read("outLanguage")
+// async function Translate()
+// {
+//     try{
+//          //1.text to translate
+// const input = read("inputText") 
+//   console.log(input)                           // .de for .com becz we are not using key
+// const inputLanguage = read("inputLanguage")
+// const outLanguage = read("outLanguage")
 
 
-const res = await fetch("https://libretranslate.de/translate",{   
-    method: "POST",
-    body: JSON.stringify({
-            q: input,
-            source:inputLanguage,
-            target:outLanguage,
-            format:"text",
-        }),
- headers:{    //if server is not showing output then we have to give header
+// const res = await fetch("https://libretranslate.de/translate",{   
+//     method: "POST",
+//     body: JSON.stringify({
+//             q: input,
+//             source:inputLanguage,
+//             target:outLanguage,
+//             format:"text",
+//         }),
+//  headers:{    //if server is not showing output then we have to give header
 
-            "Content-Type": "application/json",
-        },  
-});
+//             "Content-Type": "application/json",
+//         },  
+// });
 
-const { translatedText } = await res.json()
+// const { translatedText } = await res.json()
 
-document.getElementById("outputValue").innerText = translatedText
+// document.getElementById("outputValue").innerText = translatedText
 
-    }
+//     }
 
-    catch(err){
-  console.log(err)
-    }
+//     catch(err){
+//   console.log(err)
+//     }
    
-}
+// }
